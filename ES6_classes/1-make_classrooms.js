b34b1c5408c58ae2216ab30b87e0e5d8ceaa1096
bfrasholb <13065@holbertonstudents.com>
2026-09-15 11:51:27 +1000

    Very nice

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from'./0-classroom.js'
+
+export default function initializeRooms () {
+	let room1 = new ClassRoom(19)
+	let room2 = new ClassRoom(20)
+	let room3 = new ClassRoom(34)
+	return [room1, room2, room3]
+}
